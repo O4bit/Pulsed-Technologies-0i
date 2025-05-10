048d1b943d8439f4008ccd354ecf19ea05ddeb2e
O4bit <o4bit@protonmail.com>
2025-05-10 10:19:08 +0200

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { Github } from "lucide-react"
 
@@ -6,7 +7,14 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <Link href="/logo.svg" className="mr-6 flex items-center space-x-2">
+        <Link href="/" className="mr-6 flex items-center space-x-2">
+          <Image 
+            src="/logo.svg" 
+            alt="Pulsed Logo" 
+            width={24} 
+            height={24} 
+            className="h-6 w-6"
+          />
           <span className="font-bold text-purple-400">Pulsed, Inc.</span>
         </Link>
         <nav className="flex flex-1 items-center space-x-6 text-sm font-medium">
