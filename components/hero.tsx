3fd6c66664497776f7079d818b463b2c5bf9ed97
O4bit <o4bit@protonmail.com>
2025-06-29 15:59:32 +0200

    Did some revisions

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <section className="container flex min-h-[calc(100vh-3.5rem)] max-w-screen-2xl flex-col items-center justify-center space-y-8 py-24 text-center md:py-32">
       <div className="space-y-4">
         <h1 className="bg-gradient-to-br from-purple-300 from-30% via-purple-400 to-purple-500 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl md:text-6xl lg:text-7xl">
-          Pulsed, Inc.
+          Pulsed Inc.
           <br />
         </h1>
         <p className="mx-auto max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,11 +11,11 @@ export default function Navbar() {
           <Image 
             src="/logo.svg" 
             alt="Pulsed Logo" 
-            width={24} 
-            height={28} 
-            className="h-6 w-6"
+            width={35} 
+            height={35} 
+            className="h-6 w-8"
           />
-          <span className="font-bold text-purple-400">Pulsed, Inc.</span>
+          <span className="font-bold text-purple-400">Pulsed Inc.</span>
         </Link>
         <nav className="flex flex-1 items-center space-x-6 text-sm font-medium">
           <Link href="/about" className="transition-colors hover:text-purple-400">
@@ -25,7 +25,7 @@ export default function Navbar() {
         <div className="flex items-center space-x-4">
           <Link href="https://github.com/Pulsedinc" target="_blank" rel="noreferrer">
             <Button variant="ghost" size="icon">
-              <Github className="h-4 w-4" />
+              <Github className="h-6 w-6" />
               <span className="sr-only">GitHub</span>
             </Button>
           </Link>
